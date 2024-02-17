b0002e1dd5b8f731f890bc659fdd78051c1f6b4f
ivandudardb <i.dudar@dev-branch.com>
2024-02-17 12:58:42 +0200

    7410.master - created storybook

---
+++ web/themes/custom/customtheme/components/01-atoms/text/text/text.stories.js
@@ -0,0 +1,27 @@
+import heading from '../headings/_heading.twig';
+import date from './02-data.twig';
+import pre from './05-search.twig';
+import paragraph from './03-weather.twig';
+import code from './07-code.twig';
+
+import dateData from './blockquote.yml';
+import headingData from '../headings/headings.yml';
+import codeData from './code.yml';
+
+/**
+ * Storybook Definition.
+ */
+export default { title: 'Atoms/Text' };
+
+// Loop over items in headingData to show each one in the example below.
+const headings = headingData.map((d) => heading(d)).join('');
+
+export const headingsExamples = () => headings;
+
+export const dateExample = () => date(dateData);
+
+export const preformatted = () => pre();
+
+export const weather  = () => paragraph();
+
+export const codeExample = () => code(codeData);
