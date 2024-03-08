e0350e201781d6c466e96878a8c7e7a76b148130
ivandudardb <i.dudar@dev-branch.com>
2024-03-08 15:47:02 +0200

    7650 - display none replaced to scss fsle

--- web/themes/custom/customtheme/components/01-atoms/buttons/back-to-top.js
+++ web/themes/custom/customtheme/components/01-atoms/buttons/back-to-top.js
@@ -1,14 +1,13 @@
 (function (Drupal, once) {
   Drupal.behaviors.backToTop = {
-    attach: function (context, settings) {
+    attach: function (context) {
       let backButtonElements = once('backToTopButton', '.footer .button-back-to-top', context);
-      backButtonElements.forEach(function(backButton) {
-        backButton.style.display = 'none';
-        backButton.addEventListener('click', function (event) {
-          event.preventDefault();
-          window.scrollTo({ top: 0, behavior: 'smooth' });
+        backButtonElements.forEach(function(backButton) {
+          backButton.addEventListener('click', function (event) {
+            event.preventDefault();
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+          });
         });
-      });
 
       window.addEventListener('scroll', function() {
         let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
