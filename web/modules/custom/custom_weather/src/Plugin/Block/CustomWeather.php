ce96c3a0d3cdefb9bd14c8f27aa31462c77552e8
ivandudardb <i.dudar@dev-branch.com>
2024-03-12 14:03:06 +0200

    7677 - considered coding standards have been applied

--- web/modules/custom/custom_weather/src/Plugin/Block/CustomWeather.php
+++ web/modules/custom/custom_weather/src/Plugin/Block/CustomWeather.php
@@ -1,14 +1,26 @@
 <?php
+
 namespace Drupal\custom_weather\Plugin\Block;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use GuzzleHttp\Client;
 
+/**
+ * Provides a 'WeatherBlock' block.
+ */
 #[Block(
   id: "custom_weather_block",
   admin_label: new TranslatableMarkup("Custom Weather block"),
 )]
 
+
 class CustomWeather extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
   public function build():array {
     $weatherData = $this->getWeatherApi();
     $temp = $weatherData['main']['temp'];
@@ -22,12 +34,16 @@ class CustomWeather extends BlockBase {
     ];
   }
 
+  /**
+   * Protected function for getting array with temperature.
+   */
   protected function getWeatherApi():mixed {
-    $client = Drupal::httpClient();
+    $client = new Client();
     $request = $client->get(
       'https://api.openweathermap.org/data/2.5/weather?lat=50.7593&lon=25.3424&appid=23033cae7426d33b990b8c0c4dffe352'
     );
     $response = $request->getBody()->getContents();
     return Json::decode($response);
   }
+
 }
